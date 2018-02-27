d1a69294385d60fe8a7ef84292035e4c47f1b317
fuhuaxuan <fuhuaxuan@126.com>
2018-02-28 00:07:24 +0800

    fix-更新流程

--- 改动记录/20171226/执行脚本 - 副本.sql
+++ 改动记录/20171226/执行脚本 - 副本.sql
@@ -7,3 +7,5 @@ update wf_flow f
                        from wf_prl_stamp wf
                       where wf.flowgid = f.flowgid)
  where exists (select 1 from wf_prl_stamp wf where wf.flowgid = f.flowgid)
+
+B78EB93A60B44944B6056C174B548DD6
